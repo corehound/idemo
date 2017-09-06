be401cfc3c057dd690f8e5e0b377132107356210
Jiri Mecner <jiri.mecner@gmail.com>
2017-09-06 22:50:22 +0200

    saml autentikace

--- src/main/java/org/corehound/idemo/IdemoApplication.java
+++ src/main/java/org/corehound/idemo/IdemoApplication.java
@@ -1,19 +1,24 @@
 package org.corehound.idemo;
 
-import javax.swing.JInternalFrame;
-
 import org.corehound.idemo.domain.City;
 import org.corehound.idemo.domain.CityRepository;
 import org.corehound.idemo.domain.MeetingRoom;
 import org.corehound.idemo.domain.MeetingRoomRepository;
-import org.corehound.idemo.domain.Reservation;
 import org.corehound.idemo.domain.ReservationRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.github.ulisesbocchio.spring.boot.security.saml.annotation.EnableSAMLSSO;
+import com.github.ulisesbocchio.spring.boot.security.saml.configurer.ServiceProviderBuilder;
+import com.github.ulisesbocchio.spring.boot.security.saml.configurer.ServiceProviderConfigurerAdapter;
 
 @SpringBootApplication
+@EnableSAMLSSO
 public class IdemoApplication {
 
 	public static void main(String[] args) {
@@ -39,12 +44,51 @@ public class IdemoApplication {
 			meetingRoomRepository.save(new MeetingRoom("Manáslu",plzen));
 			meetingRoomRepository.save(new MeetingRoom("Nanga Parbat",plzen));
 			
-			Reservation jirkovaRezervace = new Reservation();
-			jirkovaRezervace.setUser("jirka");
-			jirkovaRezervace.setRoom(mountEverest);
-			
-			reservationRepository.save(jirkovaRezervace);
+//			Reservation jirkovaRezervace = new Reservation();
+//			jirkovaRezervace.setUser("jirka");
+//			jirkovaRezervace.setRoom(mountEverest);
+//			
+//			reservationRepository.save(jirkovaRezervace);
 			
 		};
-	}
+	}	
+	
+    @Configuration
+    public static class IdemoServiceProviderConfig extends ServiceProviderConfigurerAdapter {
+
+        @Override
+        public void configure(ServiceProviderBuilder serviceProvider) throws Exception {
+
+            serviceProvider
+                .metadataGenerator()
+//                .entityId("login-test")
+                .entityId("localhost-demo")
+            .and()
+                .sso()
+                .defaultSuccessURL("/home")
+            .and()
+                .logout()
+                .defaultTargetURL("/")
+            .and()
+                .metadataManager()
+//                .metadataLocations("classpath:/idp-innogy.xml")
+                .metadataLocations("classpath:/idp-ssocircle-new.xml")
+                .refreshCheckInterval(0)
+            .and()
+                .extendedMetadata()
+                .idpDiscoveryEnabled(false)
+            .and()
+                .keyManager()
+                .privateKeyDERLocation("classpath:/localhost.key.der")
+                .publicKeyPEMLocation("classpath:/localhost.cert")
+            .and()
+                .samlContextProviderLb()
+                .scheme("http")
+                .contextPath("/")
+                .serverName("localhost")
+                .serverPort(8080)
+                .includeServerPortInRequestURL(true);
+
+        }
+    }
 }
